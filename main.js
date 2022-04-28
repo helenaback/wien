ed6afc0dd3896a968c5bcaac99eb2925404f15e5
Helena Back <helena.back@student.uibk.ac.at>
2022-04-28 17:32:21 +0200

    Map erstellt

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 /* OGD Wien Beispiel */
+
+let stephansdom = {
+    lat: 48.208493, 
+    lng: 16.373118,
+    title: "Stephansdom"
+
+};
+
+let starLayer = L.tileLayer.provider("BasemapAT.grau");
+
+/*Verweis auf div im html*/
+let map = L.map("map", {
+    center: [ stephansdom.lat, stephansdom.lng],
+    zoom: 12, 
+    layers: [starLayer
+    ],
+
+});
+
+let layerControl = L.control.layers({
+    "BasemapAT Grau": startLayer, 
+}).addTo(map);
