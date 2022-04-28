b76f6eb82b7540c7393c20be4168d48ef16b43fe
Helena Back <helena.back@student.uibk.ac.at>
2022-04-28 17:53:00 +0200

    Layer control erstelle

--- main.js
+++ main.js
@@ -7,17 +7,37 @@ let stephansdom = {
 
 };
 
-let starLayer = L.tileLayer.provider("BasemapAT.grau");
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
 
 /*Verweis auf div im html*/
 let map = L.map("map", {
     center: [ stephansdom.lat, stephansdom.lng],
     zoom: 12, 
-    layers: [starLayer
+    layers: [startLayer
     ],
 
 });
 
 let layerControl = L.control.layers({
     "BasemapAT Grau": startLayer, 
+    "Basemap Standard": L.tileLayer.provider("BasemapAT.basemap"), 
+    "Basemap Overlay": L.tileLayer.provider("BasemapAT.overlay"),
+    "Basemap Gelände": L.tileLayer.provider("BasemapAT.terrain"),
+    "Basemap Surface": L.tileLayer.provider("BasemapAT.surface"),
+    "Basemap Orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "Basemap Terrain": L.tileLayer.provider("BasemapAT.terrain"),
+    "Basemap High-DPI": L.tileLayer.provider("BasemapAT.highdpi"),
+    "Basemap mit Orthofoto und Beschriftung": L.layerGroup ([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])
+    
 }).addTo(map);
+
+layerControl.expand();
+
+let sightLayer = L.featureGroup();
+
+layerControl.addOverlay(sightLayer, "Sehenswürdigkeiten");
+
+let mrk = L.marker([stephansdom.lat, stephansdom.lng]).addTo(sightLayer);
