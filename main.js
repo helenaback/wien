03799fd7fe9ddcff430e529b053056a3f5fea0e1
Helena Back <helena.back@student.uibk.ac.at>
2022-05-12 18:11:02 +0200

    alle hotelmarker durchgehen un deigenschaften anzeigen

--- main.js
+++ main.js
@@ -227,7 +227,7 @@ async function loadHotels(url) {
     layerControl.addOverlay(overlay, "Hotels und Unterkünfte");
     overlay.addTo(map);
 
-    L.geoJSON(geojson, {
+    let hotelsLayer = L.geoJSON(geojson, {
         pointToLayer: function (geoJsonPoint, latlng) {
             //L.marker(latlng).addTo(map)
             
@@ -281,7 +281,15 @@ async function loadHotels(url) {
     let form = document.querySelector("#searchForm")
     console.log(form.hotel);
     form.suchen.onclick = function() {
-        console.log(form.hotel.value)
+        console.log(form.hotel.value);
+        hotelsLayer.eachLayer(function(marker){
+            console.log(marker)
+            console.log(marker.getLatLng())
+            console.log(marker.getPopup())
+            console.log(marker.feature.properties.BETRIEB)
+
+
+        })
     }
 
 }
